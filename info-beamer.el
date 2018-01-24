89e26607ce81d2ad8d7e2cf708ebfc915bdf7bd3
Daniel Kraus <daniel@kraus.my>
2018-01-24 14:44:18 +0800

    Add info-beamer-osc and info-beamer-input

--- info-beamer.el
+++ info-beamer.el
@@ -4,7 +4,7 @@
 
 ;; Author: Daniel Kraus <daniel@kraus.my>
 ;; Version: 0.1
-;; Package-Requires: ((request "0.3.0") (emacs "24.4"))
+;; Package-Requires: ((osc "0.1") (emacs "24.4"))
 ;; Keywords: tools, processes, comm
 ;; URL: https://github.com/dakra/info-beamer.el
 
@@ -49,31 +49,98 @@
   :safe #'integerp
   :group 'info-beamer)
 
-(defvar info-beamer-network-process nil)
+(defcustom info-beamer-tcp-host "127.0.0.1"
+  "Info beamer TCP host."
+  :type 'string
+  :safe #'stringp
+  :group 'info-beamer)
+
+(defcustom info-beamer-tcp-port 4444
+  "Info beamer TCP port."
+  :type 'integer
+  :safe #'integerp
+  :group 'info-beamer)
+
+(defvar info-beamer-udp-process nil)
+(defvar info-beamer-tcp-process nil)
 
 (defun info-beamer-get-current-node ()
   "Return current directory."
   (file-name-nondirectory (directory-file-name default-directory)))
 
-(defun info-beamer-get-network-process ()
-  "Get info-beamer network connection."
-  (if (and info-beamer-network-process (process-live-p info-beamer-network-process))
-      info-beamer-network-process
-    (setq info-beamer-network-process
+(defun info-beamer-get-udp-process ()
+  "Get info-beamer UDP network connection."
+  (if (and info-beamer-udp-process (process-live-p info-beamer-udp-process))
+      info-beamer-udp-process
+    (setq info-beamer-udp-process
           (make-network-process
-           :name "info-beamer-udp-socket"
+           :name "info-beamer-udp-process"
            :host info-beamer-udp-host
            :service info-beamer-udp-port
            :type 'datagram))))
 
+(defun info-beamer-delete-udp-process ()
+  "Deletes UDP process to info-beamer."
+  (interactive)
+  (when (and info-beamer-udp-process (process-live-p info-beamer-udp-process))
+    (delete-process info-beamer-udp-process)))
+
+(defun info-beamer-get-tcp-process ()
+  "Get info-beamer TCP network connection."
+  (if (and info-beamer-tcp-process (process-live-p info-beamer-tcp-process))
+      info-beamer-tcp-process
+    (setq info-beamer-tcp-process
+          (make-network-process
+           :name "info-beamer-tcp-process"
+           :host info-beamer-tcp-host
+           :service info-beamer-tcp-port
+           :filter #'info-beamer-tcp-listen-filter))))
+
+(defun info-beamer-tcp-listen-filter (_proc str)
+  "Log output STR from info-beamer TCP connection."
+  (message "Info-beamer: %s" str))
+
+(defun info-beamer-disconnect ()
+  "Deletes TCP process to info-beamer."
+  (interactive)
+  (when (and info-beamer-tcp-process (process-live-p info-beamer-tcp-process))
+    (delete-process info-beamer-tcp-process)))
+
 ;;;###autoload
-(defun info-beamer-send-string (data &optional path)
-  "Send DATA to info-beamer PATH."
-  (interactive "sSend string to info beamer: ")
-  (let* ((process (info-beamer-get-network-process))
-         (path (or path (info-beamer-get-current-node)))
-         (str (format "%s:%s" path data)))
-    (process-send-string process str)))
+(defun info-beamer-connect (&optional node)
+  "Connect to info-beamer node NODE."
+  (interactive)
+  (let ((node (or node (info-beamer-get-current-node))))
+    (info-beamer-input node)))
+
+;;;###autoload
+(defun info-beamer-input (line)
+  "Send LINE via TCP to info-beamer."
+  (interactive "sSend line to info beamer: ")
+  (let ((process (info-beamer-get-tcp-process)))
+    (process-send-string process (concat line "\n"))))
+
+;;;###autoload
+(defun info-beamer-data (data &optional node)
+  "Send DATA via UDP to info-beamer NODE.
+If NODE is NIL use current directory."
+  (interactive "sSend data to info beamer: ")
+  (let* ((process (info-beamer-get-udp-process))
+         (node (or node (info-beamer-get-current-node)))
+         (path (format "%s:%s" node data)))
+    (process-send-string process path)))
+
+;;;###autoload
+(defun info-beamer-osc (suffix &optional node &rest args)
+  "Send OSC packet with ARGS to path /NODE/SUFFIX."
+  (interactive "sSend OSC packet to path: ")
+  (if (require 'osc nil t)
+      (let* ((process (info-beamer-get-udp-process))
+             (node (or node (info-beamer-get-current-node)))
+             (path (format "/%s/%s" node suffix)))
+        (message "Sending OSC message to %s" path)
+        (apply 'osc-send-message process path args))
+    (error "You need to install OSC to send OSC packets")))
 
 ;;;###autoload
 (defun info-beamer-run (&optional node)
@@ -84,7 +151,7 @@
          (buf-name (format "info-beamer %s%s"
                            (info-beamer-get-current-node)
                            (if node (format "/%s" node) ""))))
-    (compilation-start command nil (lambda (m) buf-name))))
+    (compilation-start command nil (lambda (_mode) buf-name))))
 
 (provide 'info-beamer)
 ;;; info-beamer.el ends here
